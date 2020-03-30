eac263ad0eea9989cdcc36f35187d457fce6c466
Jeroen Vreeken <jeroen@vreeken.net>
2020-03-30 23:47:30 +0200

    Add unittests for mode 6000

---
+++ unittest/tmode6000.c
@@ -0,0 +1,255 @@
+/*
+  Copyright (C) 2020 Jeroen Vreeken <jeroen@vreeken.net>
+
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program; if not, see <http://www.gnu.org/licenses/>.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "mode6000.h"
+
+bool rx_check = true;
+bool rx_check_failed = false;
+bool const_check_failed = false;
+
+int demod_sync_nr = 0;
+int data_channel_rx_8 = 0;
+int data_channel_rx_76 = 0;
+int data_channel_rx_ok_8 = 0;
+int data_channel_rx_ok_76 = 0;
+int voice_test_nr = 0;
+
+void freedv_data_channel_rx_frame(struct freedv_data_channel *fdc, unsigned char *data, size_t size, int from_bit, int bcast_bit, int crc_bit, int end_bits)
+{
+	printf("freedv_data_channel_rx_frame(%p, %p, %zd)\n", fdc, data, size);
+	
+	int i;
+	unsigned char testval = 0;
+	int ok = 1;
+	
+	for (i = 0; i < size; i++) {
+		unsigned char val = data[i];
+		printf("%02x", val);
+		
+		if (rx_check) {
+			if (val != testval) {
+				printf(" data byte %d: 0x%02x does not match calculated 0x%02x\n", i, val, testval);
+				rx_check_failed = true;
+				ok = 0;
+			}
+		}
+		
+		testval += size;
+	}
+	
+	if (rx_check) {
+		if (end_bits != size) {
+			printf("end_bits %d invalid\n", end_bits);
+			rx_check_failed = true;
+		}
+		if (from_bit != 0) {
+			printf("from_bit %d invalid\n", from_bit);
+			rx_check_failed = true;
+		}
+		if (bcast_bit != 1) {
+			printf("bcast_bit %d invalid\n", bcast_bit);
+			rx_check_failed = true;
+		}
+	}
+	
+	if (size == 8) {
+		data_channel_rx_8++;
+		data_channel_rx_ok_8 += ok;
+	}
+	if (size == 76) {
+		data_channel_rx_76++;
+		data_channel_rx_ok_76 += ok;
+	}
+	
+	printf(" %d %d %d\n", from_bit, bcast_bit, end_bits);
+}
+
+void freedv_data_channel_tx_frame(struct freedv_data_channel *fdc, unsigned char *data, size_t size, int *from_bit, int *bcast_bit, int *crc_bit, int *end_bits)
+{
+	printf("freedv_data_channel_tx_frame(%p, %p, %zd\n", fdc, data, size);
+	
+	unsigned char testval = 0;
+	int i;
+	
+	for (i = 0; i < size; i++) {
+		data[i] = testval;
+		testval += size;
+	}
+	*end_bits = size;
+	*from_bit = 0;
+	*bcast_bit = 1;
+}
+
+void m6000_test_voice_gen(unsigned char *voice)
+{
+	int i;
+	
+	for (i = 0; i < 70; i++) {
+		voice[i] = i + 'v';
+	}
+}
+
+void m6000_test_voice(unsigned char *voice)
+{
+	int i;
+	int r = 1;
+	
+	for (i = 0; i < 70; i++) {
+		if (voice[i] != i + 'v') {
+			printf("Voice byte %d value 0x%02x does not match generated pattern 0x%02x\n", i, voice[i], i + 'v');
+			rx_check_failed = true;
+			r = 0;
+		}
+	}
+	voice_test_nr += r;
+}
+
+#define NR_FRAMES 100
+
+int main(int argc, char **argv)
+{
+	struct m6000 *m6000 = m6000_create();
+
+	printf("Mode 6000 modem tests\n");
+	
+	printf("Check m6000_get_modem_sample_rate() ");
+	int m_rate = m6000_get_modem_sample_rate(m6000);
+	if (m_rate != 48000) {
+		printf("modemrate is not 48000\n");
+		const_check_failed = true;
+	} else {
+		printf("Passed\n");
+	}
+	
+	printf("m6000_get_modem_symbol_rate() ");
+	int m_symbolrate = m6000_get_modem_symbol_rate(m6000);
+	if (m_symbolrate != 6000) {
+		printf("symbolrate is not 6000\n");
+		const_check_failed = true;
+	} else {
+		printf("Passed\n");
+	}
+
+	printf("m6000_get_n_nom_modem_samples() ");
+	int m_framesize = m6000_get_n_nom_modem_samples(m6000);
+	if (m_framesize != 5760) {
+		printf("framesize is not 5760: %d\n", m_framesize);
+		const_check_failed = true;
+	} else {
+		printf("Passed\n");
+	}
+
+	printf("m6000_get_codec_bytes() ");
+	int codec_bytes = m6000_get_codec_bytes(m6000);
+	if (codec_bytes != 70) {
+		printf("codec bytes is not 70: %d\n", codec_bytes);
+		const_check_failed = true;
+	} else {
+		printf("Passed\n");
+	}
+	
+	int i;
+	
+	bool test_mod = false;
+	bool test_demod = false;
+	
+	if (argc >= 2) {
+		if (!strcmp(argv[1], "demod")) {
+			test_demod = true;
+			test_mod = false;
+		}
+		if (!strcmp(argv[1], "mod")) {
+			test_demod = false;
+			test_mod = true;
+		}
+	}
+	if (test_demod) {
+		printf("Executing demod test: provide input on stdin\n");
+	} else {
+		printf("Skipping demod test, to execute run: 'tmode6000 demod <testdata.raw'\n");
+	}
+	if (test_demod) {
+		printf("Executing mod test: Test data generated to stderr\n");
+	} else {
+		printf("Skipping mod test, to execute run: 'tmode6000 mod 2>testdata.raw'\n");
+	}
+	
+	short frame[m6000_get_n_max_modem_samples(m6000)];
+	unsigned char voice[70] = {0};
+	if (test_mod) {
+		for (i = 0; i < NR_FRAMES; i++) {
+			if (i&1) {
+				m6000_mod_data(m6000, NULL, frame);
+			} else {
+				m6000_test_voice_gen(voice);
+				m6000_mod_codec(m6000, NULL, frame, voice);
+			}
+			
+			fwrite(frame, m6000_get_n_nom_modem_samples(m6000), sizeof(short), stderr);
+		}
+	}
+	
+	if (test_demod) {
+		bool cont = true;
+		while (cont) {
+			int nin = m6000_nin(m6000);
+			printf("Read %d samples\n", nin);
+			int r = fread(frame, sizeof(short), nin, stdin);
+			cont = (r == nin);
+
+			int r_d = m6000_demod(m6000, NULL, frame, voice);
+			
+			printf("demod: %d\n", r_d);
+			if (r_d)
+				m6000_test_voice(voice);
+			
+			int sync;
+			float snr_est;
+			m6000_get_modem_stats(m6000, &sync, &snr_est);
+
+			if (sync)
+				demod_sync_nr++;
+		
+			printf("sync: %d snr_est: %f\n", sync, snr_est);
+		}
+	}
+	
+	if (test_mod) {
+		printf("Generated %d frames\n", NR_FRAMES);
+	}
+	if (test_demod) {
+		printf("Demod was synced for %d frames\n", demod_sync_nr);
+		printf("Demod received %d (out of %d) frames with 8 data bytes\n", 
+		    data_channel_rx_ok_8, data_channel_rx_8);
+		printf("Demod received %d (out of %d) frames with 76 data bytes\n", 
+		    data_channel_rx_ok_76, data_channel_rx_76);
+		printf("Demod received %d correct voice frames\n", voice_test_nr);
+	}
+
+	if (rx_check_failed || const_check_failed) {
+		printf("Test failed\n");
+		return 1;
+	}
+	printf("Test passed\n");
+
+	return 0;
+}
+
